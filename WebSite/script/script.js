7810c03a355d94242760f71a3e153c3d89383c85
WalidMoua <walidmou97@gmail.com>
2022-05-26 12:08:51 +0200

    adding bookign form  and script

--- WebSite/script/script.js
+++ WebSite/script/script.js
@@ -26,25 +26,84 @@ const weekName = [
 const d = new Date();
 const tomorrow = new Date();
 tomorrow.setDate(tomorrow.getDate() + 1);
+let startDate = d;
+let endDate;
 
-$("#today-date").append(
-  `<h4 class="fs-6 text-blue">` +
-    weekName[d.getDay()] +
-    `</h4>
-  <p class="fs-3">` +
-    d.getDate() +
-    monthNames[d.getMonth()] +
-    `</p>`
-);
+function changeStartDate(event) {
+  let d = new Date(event.target.value);
+  let dYesterday = new Date();
+  dYesterday.setDate(tomorrow.getDate() - 2);
 
-$("#tomorrow-date").append(
-  `<h4 class="fs-6 text-blue">` +
-    weekName[tomorrow.getDay()] +
-    `</h4>
+  if (Date.parse(d) < Date.parse(dYesterday)) {
+    alert("CHOSE A DATE AFTER TODAY'S DATE");
+  } else {
+    if (Date.parse(endDate) < Date.parse(d)) {
+      alert("CHOSE A CHECK-OUT DATE AFTER THE CHECK-IN ");
+    } else {
+      startDate = d;
+      $(".today-date").html(
+        `<h4 class="fs-6 text-blue">` +
+          weekName[d.getDay()] +
+          `</h4>
+        <p class="fs-3">` +
+          d.getDate() +
+          monthNames[d.getMonth()] +
+          `</p>`
+      );
+    }
+  }
+}
+
+function changeEndDate(event) {
+  let d = new Date(event.target.value);
+
+  if (Date.parse(d) < Date.parse(startDate)) {
+    console.log(d, " a  ", startDate);
+    alert("CHOSE A DATE AFTER TODAY'S DATE");
+  } else {
+    endDate = d;
+    $(".tomorrow-date").html(
+      `<h4 class="fs-6 text-blue">` +
+        weekName[d.getDay()] +
+        `</h4>
+      <p class="fs-3">` +
+        d.getDate() +
+        monthNames[d.getMonth()] +
+        `</p>`
+    );
+  }
+}
+
+function setBaseDates() {
+  $(".today-date").append(
+    `<h4 class="fs-6 text-blue">` +
+      weekName[d.getDay()] +
+      `</h4>
     <p class="fs-3">` +
-    tomorrow.getDate() +
-    monthNames[tomorrow.getMonth()] +
-    `</p>`
-);
+      d.getDate() +
+      monthNames[d.getMonth()] +
+      `</p>`
+  );
 
-$(document).ready(function () {});
+  $(".tomorrow-date").append(
+    `<h4 class="fs-6 text-blue">` +
+      weekName[tomorrow.getDay()] +
+      `</h4>
+      <p class="fs-3">` +
+      tomorrow.getDate() +
+      monthNames[tomorrow.getMonth()] +
+      `</p>`
+  );
+}
+
+$("#start-date").click(function () {
+  var input = document.querySelector("#start-date-picker");
+  input.focus();
+  input.click();
+
+  console.log("cliked");
+});
+
+$(document).ready(function () {
+  setBaseDates();
+});
